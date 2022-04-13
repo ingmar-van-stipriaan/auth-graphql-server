f768b0d603f5c1b99840b1fc9379f7bd72f7a2ef
Ingmar <ingmar.stipriaan@bettyblocks.com>
2022-04-13 08:27:06 +0200

    first commit

---
+++ server/schema/types/root_query_type.js
@@ -0,0 +1,11 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const RootQueryType = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    dummyField: { type: GraphQLString },
+  },
+});
+
+module.exports = RootQueryType;
