a3535514ffb43b675b9a6ed2f91ae438f0c17e6a
Ingmar <i_ngmar@hotmail.com>
2022-04-18 20:30:43 +0200

    add logged out state buttons

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
 import { graphql } from "@apollo/client/react/hoc";
 import query from "../queries/CurrentUser";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, currentUser } = this.props.data;
+    if (loading) {
+      return <div />;
+    }
+    if (currentUser) {
+      return <div>Logout</div>;
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
+  }
+
   render() {
-    console.log(this.props.data);
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
